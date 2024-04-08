0b169a2dc9628b7ddcfbbaee5837b1b4d66c2b73
kali <kali@kali.itsec-lab-emcl.iwr.uni-heidelberg.de>
2024-04-08 10:58:16 -0400

    anpassung

--- project_proxy_reliability/api.py
+++ project_proxy_reliability/api.py
@@ -15,15 +15,13 @@ def handshake(ip, port, proxy_list,counter):
 # Create SYN-Paket to Proxy
     syn_packet = IP(dst=target_ip) / TCP(dport=target_port, flags="S")
     print("Erstelle SYN- Paket: \n")
-    syn_packet.show()
+    #syn_packet.show()
 
 # Transceive SYN-Paket and Receive Answer
     print("Sendet SYN- Paket\n")
     syn_ack_response = sr1(syn_packet, timeout=2, verbose=False)
     
-    
 
-    
 
     if syn_ack_response:
         syn_ack_time = syn_ack_response.time - syn_packet.sent_time
@@ -40,8 +38,8 @@ def handshake(ip, port, proxy_list,counter):
             
         #Display Paket Answer to console
 
-            print("Die Antwort:\n")
-            syn_ack_response.show() 
+            #print("Die Antwort:\n")
+            #syn_ack_response.show() 
 
         # Send ACK-Paket to Proxy
             send(ack_packet)
@@ -51,7 +49,7 @@ def handshake(ip, port, proxy_list,counter):
                 if elements.get("ip") == target_ip and elements.get("port") == target_port:
                     
                     elements["log_handshake"].append(1)#log successful handshake
-                    elements["syn_ack_time"] = syn_ack_time
+                    elements["log_syn_ack_time"].append(syn_ack_time)
                     x = elements.get("log_handshake")
                     print(f"Log Handshake set to \n {x}\n")
         else:
@@ -59,7 +57,7 @@ def handshake(ip, port, proxy_list,counter):
             
             for elements in proxy_list:
                 if elements.get("ip") == target_ip and elements.get("port") == target_port:
-                    elements["syn_ack_time"] = syn_ack_time
+                    elements["log_syn_ack_time"].append(syn_ack_time)
                     elements["log_handshake"].append(0)#log unsuccessful handshake
                     x = elements.get("log_handshake")
                     print(f"Log Handshake set to \n {x}\n")
@@ -71,39 +69,44 @@ def handshake(ip, port, proxy_list,counter):
             if elements.get("ip") == target_ip and elements.get("port") == target_port:
                     
                     elements["log_handshake"].append(0)#log unsuccessful handshake
+                    elements["log_syn_ack_time"].append(0)
                     x = elements.get("log_handshake")
                     print(f"Log Handshake set to \n {x}\n")
 
 def calc_score(proxy_list,input_handshake_tries):
-    #Calculate TCP-Handshake Score
+    
     for elements in proxy_list:
+    #Calculate TCP-Handshake Score
         succ_handshakes = elements["log_handshake"].count(1)
         handshake_rate = succ_handshakes / input_handshake_tries
         score = handshake_rate * 100
         elements["score"]= score
-        
-
-
-    #TODO Add Bonus of 3 Best Avg_resp_time to score ; 15 , 10 , 5 points
-
-      
-        
-    """
-    proxy_list_copy = proxy_list
-    ranked_list = []
+    #TODO Calculate SYN-ACK Score Add Bonus of 3 Best Avg_resp_time to score ; 15 , 10 , 5 points
+        sum_syn_ack = sum(elements["log_syn_ack_time"])
+        avg_syn_ack_time = sum_syn_ack / input_handshake_tries
+        elements["avg_syn_ack_time"] = avg_syn_ack_time
+        elements["log_syn_ack_time"].clear()
+        if elements["avg_syn_ack_time"] == 0.0:
+            proxy_list.remove(elements)
     
-    for i in range(0,2):
-        max_global= 1
-        best_proxy = 0
-        for elements in proxy_list_copy:
-            max_local = elements.get("avg_resp_time")
-            if max_local < max_global:
-                ranked_list.append(elements)
-                proxy_list_copy.remove(elements)
-                max_global = max_local
-            else:
-                max_global = max_local
-    """
+    
+    print("Vor SYN ACK Sortierung \n")
+    print_proxy_list_dict(proxy_list)
+    
+
+    proxy_list.sort(key=lambda e: e["avg_syn_ack_time"], reverse=False) 
+    print("Nach SYN-ACK Sortierung\n")
+    print_proxy_list_dict(proxy_list)
+    proxy_list[0]["score"] += 15
+    proxy_list[1]["score"] += 10
+    proxy_list[2]["score"] += 5
+    print("Nach SYN ACK SCore Anpassung\n")
+    print_proxy_list_dict(proxy_list)
+        
+    #TODO Speed Score calculation
+
+    #TODO Throughput Score calculation
+
 
 def handshake_call(proxy_list,counter, input_handshake_tries):
   while counter < input_handshake_tries: 
@@ -118,7 +121,7 @@ def handshake_call(proxy_list,counter, input_handshake_tries):
         targetport = int(targetport)
         handshake(targetip,targetport,proxy_list,counter) # Perform the TCP-Handshake with the proxy.
         
-    #TODO is_proxy_working mit Score anpassen
+    
     calc_score(proxy_list,input_handshake_tries)  
     print_proxy_list_dict(proxy_list) 
 
@@ -141,7 +144,7 @@ async def write_proxy_to_dict(input_number,proxies, proxy_list,input_handshake_t
             proxy_list[proxycount]["ip"]=ip
             proxy_list[proxycount]["port"]=port
             proxy_list[proxycount]["handshakes"]= input_handshake_tries
-            proxy_list[proxycount]["avg_resp_time"]=avg_response_time
+            
             
 
             x = proxy_list[proxycount].items()
@@ -165,10 +168,12 @@ def init_proxy_list(input_number,proxy_list):
         proxy_data = {"ip" : 0,
                     "port" :0,
                     "score" : 0,
+                    "avg_syn_ack_time" : 0,
                     "handshakes" : 0,
                     "log_handshake": [],
-                    "syn_ack_time" : 0,
-                    "avg_resp_time" : 0
+                    "log_syn_ack_time" : [],
+                    
+                    
                     
                     
                     }
@@ -177,10 +182,7 @@ def init_proxy_list(input_number,proxy_list):
 def balance_proxy_list(proxy_list):
     # SORT List given the "score"-Field if a Proxy with score 100 is already available else --> FIND
     #TODO SORT first that proxy with score 100 is on top
-    print("Vor Sortierung:\n")
-
-    print_proxy_list_dict(proxy_list)
-
+    
     proxy_list.sort(key=lambda e: e["score"], reverse=True)
 
     
